37c8cb3e88d4be00ffd2cb91523b7ccf294d6c3f
Anjan Roy <hello@itzmeanjan.in>
2022-11-12 10:52:53 +0400

    added routine for generating pseudorandom keys using shake256
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/hashing.hpp
+++ include/hashing.hpp
@@ -8,6 +8,9 @@ namespace sphincs_hashing {
 // Given n -bytes root, n -bytes public key seed, n -bytes public key root and
 // mlen -bytes message ( to be signed ), this routine uses SHAKE256, as a keyed
 // hash function, for compressing message, while extracting out m -bytes output
+//
+// See section 7.2.1 of Sphincs+ specification
+// https://sphincs.org/data/sphincs+-r3.1-specification.pdf
 template<const size_t n, const size_t m>
 inline static void
 h_msg(const uint8_t* const __restrict r,
@@ -27,6 +30,30 @@ h_msg(const uint8_t* const __restrict r,
   hasher.finalize();
 
   hasher.read(dig, m);
+}
+
+// Given n -bytes public key seed, n -bytes secret key seed and 32 -bytes
+// address, this routine makes use of SHAKE256, as pseudorandom function, for
+// generating pseudorandom key of byte length n
+//
+// See section 7.2.1 of Sphincs+ specification
+// https://sphincs.org/data/sphincs+-r3.1-specification.pdf
+template<const size_t n>
+inline static void
+prf(const uint8_t* const __restrict pk_seed,
+    const uint8_t* const __restrict sk_seed,
+    const uint8_t* const __restrict adrs,
+    uint8_t* const __restrict dig)
+{
+  shake256::shake256<true> hasher{};
+
+  hasher.absorb(pk_seed, n);
+  hasher.absorb(adrs, 32);
+  hasher.absorb(sk_seed, n);
+
+  hasher.finalize();
+
+  hasher.read(dig, n);
 }
 
 }
