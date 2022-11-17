ef366f30d3058e50794ef3bfb3c49d6831876eab
Anjan Roy <hello@itzmeanjan.in>
2022-11-17 11:44:53 +0400

    utility routine for ease of converting byte array to hex string
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <cstddef>
 #include <cstdint>
+#include <iomanip>
+#include <sstream>
 
 // Utility functions for SPHINCS+
 namespace sphincs_utils {
@@ -35,6 +37,21 @@ from_be_bytes(const uint8_t* const bytes)
          (static_cast<uint32_t>(bytes[1]) << 16) |
          (static_cast<uint32_t>(bytes[2]) << 8) |
          (static_cast<uint32_t>(bytes[3]) << 0);
+}
+
+// Given a bytearray of length N, this function converts it to human readable
+// hex string of length N << 1 | N >= 0
+static inline const std::string
+to_hex(const uint8_t* const bytes, const size_t len)
+{
+  std::stringstream ss;
+  ss << std::hex;
+
+  for (size_t i = 0; i < len; i++) {
+    ss << std::setw(2) << std::setfill('0') << static_cast<uint32_t>(bytes[i]);
+  }
+
+  return ss.str();
 }
 
 }
