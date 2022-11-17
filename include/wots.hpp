b7fdf7a0d83ae1efe6ef3fca8558e49d2d83f93c
Anjan Roy <hello@itzmeanjan.in>
2022-11-17 19:01:53 +0400

    compute WOTS+ compressed public key from message and signature
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/wots.hpp
+++ include/wots.hpp
@@ -108,12 +108,12 @@ sign(const uint8_t* const __restrict msg,     // n -bytes message to sign
   constexpr size_t len = len1 + len2;
 
   uint32_t csum = 0;
-  uint8_t msg_[len]{};
+  uint8_t tmp[len]{};
 
-  sphincs_utils::base_w<w, len1>(msg, n, msg_);
+  sphincs_utils::base_w<w, len1>(msg, n, tmp);
 
   for (size_t i = 0; i < len1; i++) {
-    csum += static_cast<uint32_t>(w - 1ul) - static_cast<uint32_t>(msg_[i]);
+    csum += static_cast<uint32_t>(w - 1ul) - static_cast<uint32_t>(tmp[i]);
   }
 
   if constexpr ((lgw & 7ul) != 0) {
@@ -125,7 +125,7 @@ sign(const uint8_t* const __restrict msg,     // n -bytes message to sign
   constexpr size_t len_2_bytes = t1 >> 3; // = ceil(t0 / 8)
 
   const auto bytes = sphincs_utils::to_byte<uint32_t, len_2_bytes>(csum);
-  sphincs_utils::base_w<w, len2>(bytes.data(), len_2_bytes, msg_ + len1);
+  sphincs_utils::base_w<w, len2>(bytes.data(), len_2_bytes, tmp + len1);
 
   sphincs_adrs::wots_prf_t sk_adrs{ adrs };
 
@@ -145,9 +145,70 @@ sign(const uint8_t* const __restrict msg,     // n -bytes message to sign
     adrs.set_chain_address(i);
     adrs.set_hash_address(0);
 
-    const uint32_t steps = static_cast<uint32_t>(msg[i]);
+    const uint32_t steps = static_cast<uint32_t>(tmp[i]);
     chain<n, w, v>(sk, 0u, steps, adrs, pk_seed, sig + off);
   }
+}
+
+// Computes n -bytes WOTS+ compressed public key from n -bytes message and
+// n * len -bytes signature, when n -bytes public key seed and 32 -bytes WOTS+
+// hash address is also provided, using algorithm 6, defined in section 3.6 of
+// SPHINCS+ specification
+// https://sphincs.org/data/sphincs+-r3.1-specification.pdf
+template<const size_t n, const size_t w, const sphincs_hashing::variant v>
+inline static void
+pk_from_sig(
+  const uint8_t* const __restrict sig, // n * len -bytes signature
+  const uint8_t* const __restrict msg, // n -bytes message ( which was signed )
+  const uint8_t* const __restrict pk_seed, // n -bytes public key seed
+  sphincs_adrs::wots_hash_t adrs,          // 32 -bytes WOTS+ hash address
+  uint8_t* const __restrict pkey           // n -bytes public key
+)
+{
+  constexpr size_t lgw = sphincs_utils::log2<w>();
+  constexpr size_t len1 = sphincs_utils::compute_wots_len1<n, w>();
+  constexpr size_t len2 = sphincs_utils::compute_wots_len2<n, w, len1>();
+  constexpr size_t len = len1 + len2;
+
+  sphincs_adrs::wots_pk_t pk_adrs{ adrs };
+
+  uint32_t csum = 0;
+  uint8_t tmp0[len]{};
+
+  sphincs_utils::base_w<w, len1>(msg, n, tmp0);
+
+  for (size_t i = 0; i < len1; i++) {
+    csum += static_cast<uint32_t>(w - 1ul) - static_cast<uint32_t>(tmp0[i]);
+  }
+
+  if constexpr ((lgw & 7ul) != 0) {
+    csum <<= (8ul - ((len2 * lgw) & 7ul));
+  }
+
+  constexpr size_t t0 = len2 * lgw;
+  constexpr size_t t1 = t0 + 7ul;
+  constexpr size_t len_2_bytes = t1 >> 3; // = ceil(t0 / 8)
+
+  const auto bytes = sphincs_utils::to_byte<uint32_t, len_2_bytes>(csum);
+  sphincs_utils::base_w<w, len2>(bytes.data(), len_2_bytes, tmp0 + len1);
+
+  uint8_t tmp1[n * len]{};
+
+  for (uint32_t i = 0; i < static_cast<uint32_t>(len); i++) {
+    const size_t off = static_cast<size_t>(i) * n;
+
+    adrs.set_chain_address(i);
+    adrs.set_hash_address(0);
+
+    const uint32_t sidx = static_cast<uint32_t>(tmp0[i]);
+    const uint32_t steps = static_cast<uint32_t>((w - 1) - tmp0[i]);
+    chain<n, w, v>(sig + off, sidx, steps, adrs, pk_seed, tmp1 + off);
+  }
+
+  pk_adrs.set_type(sphincs_adrs::type_t::WOTS_PK);
+  pk_adrs.set_keypair_address(adrs.get_keypair_address());
+
+  sphincs_hashing::t_l<n, len, v>(pk_seed, pk_adrs.data, tmp1, pkey);
 }
 
 }
