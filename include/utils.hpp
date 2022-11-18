e965fcb611580461962ee4050c47eb93e697cc71
Anjan Roy <hello@itzmeanjan.in>
2022-11-18 11:15:23 +0400

    generate random data using system randomness
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/utils.hpp
+++ include/utils.hpp
@@ -5,6 +5,7 @@
 #include <cstdint>
 #include <cstring>
 #include <iomanip>
+#include <random>
 #include <sstream>
 #include <type_traits>
 
@@ -185,6 +186,21 @@ to_hex(const uint8_t* const bytes, const size_t len)
   }
 
   return ss.str();
+}
+
+// Generates N -many random values of type T | N >= 0
+template<typename T>
+static inline void
+random_data(T* const data, const size_t len)
+  requires(std::is_unsigned_v<T>)
+{
+  std::random_device rd;
+  std::mt19937_64 gen(rd());
+  std::uniform_int_distribution<T> dis;
+
+  for (size_t i = 0; i < len; i++) {
+    data[i] = dis(gen);
+  }
 }
 
 }
