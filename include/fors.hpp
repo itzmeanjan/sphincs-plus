12561316ac9f5ccf8af0d40a583da3d7f44e5d95
Anjan Roy <hello@itzmeanjan.in>
2022-11-21 11:44:32 +0400

    compute FORS signature
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/fors.hpp
+++ include/fors.hpp
@@ -136,4 +136,48 @@ pkgen(const uint8_t* const __restrict sk_seed, // n -bytes secret key seed
   sphincs_hashing::t_l<n, k, v>(pk_seed, roots_adrs.data, roots, pkey);
 }
 
+// Computes k * n * (a + 1) -bytes FORS signature over message of ⌈(k * a) / 8⌉
+// -bytes ( i.e. more specifically k * a -bit string ), given n -bytes secret
+// key seed, n -bytes public key seed and 32 -bytes address, encapsulating
+// position of FORS instance within SPHINCS+ structure, using algorithm 17, as
+// described in section 5.5 of SPHINCS+ specification
+// https://sphincs.org/data/sphincs+-r3.1-specification.pdf
+template<const size_t n,
+         const uint32_t k,
+         const uint32_t t,
+         const uint32_t a,
+         const sphincs_hashing::variant v>
+inline static void
+sign(const uint8_t* const __restrict msg, // ⌈(k * a) / 8⌉ -bytes message
+     const uint8_t* const __restrict sk_seed, // n -bytes secret key seed
+     const uint8_t* const __restrict pk_seed, // n -bytes public key seed
+     const sphincs_adrs::fors_tree_t adrs,    // 32 -bytes FORS address
+     uint8_t* const __restrict sig // k * n * (a + 1) -bytes FORS signature
+     )
+  requires(check_ta(t, a))
+{
+  constexpr size_t skey_val_len = n;
+  constexpr size_t auth_path_len = static_cast<size_t>(a) * n;
+  constexpr size_t sig_elm_len = skey_val_len + auth_path_len;
+
+  for (uint32_t i = 0; i < k; i++) {
+    const size_t frm = i * a;
+    const size_t to = (i + 1) * a - 1;
+
+    uint32_t idx = sphincs_utils::extract_contiguous_bits_as_u32(msg, frm, to);
+
+    const size_t off0 = i * sig_elm_len;
+    const size_t off1 = off0 + skey_val_len;
+
+    skgen<n>(pk_seed, sk_seed, adrs, i * t + idx, sig + off0);
+
+    for (uint32_t j = 0; j < a; j++) {
+      const size_t off2 = off1 + j * n;
+
+      const uint32_t s = (idx >> j) ^ 1u;
+      treehash<n, v>(sk_seed, i * t + (s << j), j, pk_seed, adrs, sig + off2);
+    }
+  }
+}
+
 }
