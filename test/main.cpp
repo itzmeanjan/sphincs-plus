a8a7dcc9c0b19744fdb816af3e4e0da4b76f5251
Anjan Roy <hello@itzmeanjan.in>
2022-11-24 16:44:21 +0400

    test SPHINCS+ implementation with various parameter sets
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- test/main.cpp
+++ test/main.cpp
@@ -70,5 +70,42 @@ main()
 
   std::cout << "[test] FORS signature\n";
 
+  {
+    using namespace test_sphincs;
+    using namespace sphincs_hashing;
+
+    test_sphincs_plus<16, 63, 7, 12, 14, 16, variant::robust>(32);
+    test_sphincs_plus<16, 63, 7, 12, 14, 16, variant::robust, true>(32);
+    test_sphincs_plus<16, 63, 7, 12, 14, 16, variant::simple>(32);
+    test_sphincs_plus<16, 63, 7, 12, 14, 16, variant::simple, true>(32);
+
+    test_sphincs_plus<16, 66, 22, 6, 33, 16, variant::robust>(32);
+    test_sphincs_plus<16, 66, 22, 6, 33, 16, variant::robust, true>(32);
+    test_sphincs_plus<16, 66, 22, 6, 33, 16, variant::simple>(32);
+    test_sphincs_plus<16, 66, 22, 6, 33, 16, variant::simple, true>(32);
+
+    test_sphincs_plus<24, 63, 7, 14, 17, 16, variant::robust>(32);
+    test_sphincs_plus<24, 63, 7, 14, 17, 16, variant::robust, true>(32);
+    test_sphincs_plus<24, 63, 7, 14, 17, 16, variant::simple>(32);
+    test_sphincs_plus<24, 63, 7, 14, 17, 16, variant::simple, true>(32);
+
+    test_sphincs_plus<24, 66, 22, 8, 33, 16, variant::robust>(32);
+    test_sphincs_plus<24, 66, 22, 8, 33, 16, variant::robust, true>(32);
+    test_sphincs_plus<24, 66, 22, 8, 33, 16, variant::simple>(32);
+    test_sphincs_plus<24, 66, 22, 8, 33, 16, variant::simple, true>(32);
+
+    test_sphincs_plus<32, 64, 8, 14, 22, 16, variant::robust>(32);
+    test_sphincs_plus<32, 64, 8, 14, 22, 16, variant::robust, true>(32);
+    test_sphincs_plus<32, 64, 8, 14, 22, 16, variant::simple>(32);
+    test_sphincs_plus<32, 64, 8, 14, 22, 16, variant::simple, true>(32);
+
+    test_sphincs_plus<32, 68, 17, 9, 35, 16, variant::robust>(32);
+    test_sphincs_plus<32, 68, 17, 9, 35, 16, variant::robust, true>(32);
+    test_sphincs_plus<32, 68, 17, 9, 35, 16, variant::simple>(32);
+    test_sphincs_plus<32, 68, 17, 9, 35, 16, variant::simple, true>(32);
+
+    std::cout << "[test] SPHINCS+ signature\n";
+  }
+
   return EXIT_SUCCESS;
 }
