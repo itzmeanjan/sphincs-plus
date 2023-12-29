7e847c716bd10ea71ce2c0df4bf645953838c519
Anjan Roy <hello@itzmeanjan.in>
2023-12-29 17:38:48 +0400

    implement new pseudo random number generator using SHAKE128 Xof
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/utils.hpp
+++ include/utils.hpp
@@ -8,9 +8,6 @@
 #include <cstddef>
 #include <cstdint>
 #include <cstring>
-#include <iomanip>
-#include <random>
-#include <sstream>
 #include <type_traits>
 
 // Utility functions for SPHINCS+
@@ -277,21 +274,6 @@ from_hex(std::string_view hex)
   }
 
   return res;
-}
-
-// Generates N -many random values of type T | N >= 0
-template<typename T>
-static inline void
-random_data(T* const data, const size_t len)
-  requires(std::is_unsigned_v<T>)
-{
-  std::random_device rd;
-  std::mt19937_64 gen(rd());
-  std::uniform_int_distribution<T> dis;
-
-  for (size_t i = 0; i < len; i++) {
-    data[i] = dis(gen);
-  }
 }
 
 }
