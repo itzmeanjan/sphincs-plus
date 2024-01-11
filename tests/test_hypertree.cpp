80331c760bac88d015999bbd3e93961eb0e649bf
Anjan Roy <hello@itzmeanjan.in>
2024-01-11 13:13:25 +0400

    update tests to use new std::span based API
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- tests/test_hypertree.cpp
+++ tests/test_hypertree.cpp
@@ -1,5 +1,7 @@
 #include "hypertree.hpp"
+#include "prng.hpp"
 #include <gtest/gtest.h>
+#include <vector>
 
 // Test correctness of HyperTree implementation, in standalone mode, using
 //
@@ -18,27 +20,29 @@ test_hypertree()
   constexpr uint32_t ileaf = 0u;                // Leaf index in that XMSS tree
 
   // Input
-  uint8_t* sk_seed = static_cast<uint8_t*>(std::malloc(sizeof(uint8_t) * n));
-  uint8_t* pk_seed = static_cast<uint8_t*>(std::malloc(sizeof(uint8_t) * n));
-  uint8_t* msg = static_cast<uint8_t*>(std::malloc(sizeof(uint8_t) * n));
+  std::vector<uint8_t> sk_seed(n, 0);
+  std::vector<uint8_t> pk_seed(n, 0);
+  std::vector<uint8_t> msg(n, 0);
+
+  auto _sk_seed = std::span<uint8_t, n>(sk_seed);
+  auto _pk_seed = std::span<uint8_t, n>(pk_seed);
+  auto _msg = std::span<uint8_t, n>(msg);
 
   // Output
-  uint8_t* pkey = static_cast<uint8_t*>(std::malloc(sizeof(uint8_t) * n));
-  uint8_t* sig = static_cast<uint8_t*>(std::malloc(sizeof(uint8_t) * sig_len));
+  std::vector<uint8_t> pkey(n, 0);
+  std::vector<uint8_t> sig(sig_len, 0);
 
-  sphincs_plus_utils::random_data<uint8_t>(sk_seed, n);
-  sphincs_plus_utils::random_data<uint8_t>(pk_seed, n);
-  sphincs_plus_utils::random_data<uint8_t>(msg, n);
+  auto _pkey = std::span<uint8_t, n>(pkey);
+  auto _sig = std::span<uint8_t, sig_len>(sig);
 
-  sphincs_plus_ht::pkgen<h, d, n, w, v>(sk_seed, pk_seed, pkey);
-  sphincs_plus_ht::sign<h, d, n, w, v>(msg, sk_seed, pk_seed, itree, ileaf, sig);
-  const bool flag = sphincs_plus_ht::verify<h, d, n, w, v>(msg, sig, pk_seed, itree, ileaf, pkey);
+  prng::prng_t prng;
+  prng.read(_sk_seed);
+  prng.read(_pk_seed);
+  prng.read(_msg);
 
-  std::free(sk_seed);
-  std::free(pk_seed);
-  std::free(msg);
-  std::free(pkey);
-  std::free(sig);
+  sphincs_plus_ht::pkgen<h, d, n, w, v>(_sk_seed, _pk_seed, _pkey);
+  sphincs_plus_ht::sign<h, d, n, w, v>(_msg, _sk_seed, _pk_seed, itree, ileaf, _sig);
+  const bool flag = sphincs_plus_ht::verify<h, d, n, w, v>(_msg, _sig, _pk_seed, itree, ileaf, _pkey);
 
   EXPECT_TRUE(flag);
 }
